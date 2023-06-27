7115db646d315733f8c08473b9aab54bf0a4ac2a
Raghunath D <rndayala@gmail.com>
2023-06-27 22:25:34 +0530

    comments added

--- Java-Examples/CoreJavaExamples/src/com/rndayala/corejava/basics/HelloWorld.java
+++ Java-Examples/CoreJavaExamples/src/com/rndayala/corejava/basics/HelloWorld.java
@@ -1,7 +1,14 @@
 package com.rndayala.corejava.basics;
 
+// Author: Raghunath Dayala
+
+// Hello,World java program
+// typically make your class as public
 public class HelloWorld {
 
+	// this is entry point for your program
+	// public - so that the method can be accessed outside
+	// static - so that it is not needed to create an object
 	public static void main(String[] args) {
 		System.out.println("Hello,World!");
 	}
