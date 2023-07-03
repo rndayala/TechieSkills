63409eb2f682e93baf88b00e8c71b8c13598411d
Raghunath D <rndayala@gmail.com>
2023-07-03 13:58:34 +0530

    factory class javadoc comment added

--- Java-Examples/CoreJavaExamples/src/com/rndayala/designpatterns/factory/FactoryClass.java
+++ Java-Examples/CoreJavaExamples/src/com/rndayala/designpatterns/factory/FactoryClass.java
@@ -1,20 +1,18 @@
 package com.rndayala.designpatterns.factory;
 
-// Author : Raghunath Dayala
-
-/* Pattern. Factory is a design pattern that provides an interface  for creating objects in a superclass, 
+/**
+ * @author rndayala
+ * Factory is a design pattern that provides an interface  for creating objects in a superclass,
  * but allows subclasses to alter the type of objects that will be  created.
  * 
  * This pattern is used to create objects without specifying the exact class of object that will be created.
- * 
- * In the Factory design pattern, you have a factory class that creates objects of different types. 
+ * In the Factory design pattern, you have a factory class that creates objects of different types.
  * When you ask the factory to create an object, you specify the type of object you want.
  * 
  * Think of factory as "object making machine". You tell factory what you want, and it creates the object for you.
- * 
- * The Factory Design Pattern is used in situations where you need to 'create objects of similar types', 
+ * The Factory Design Pattern is used in situations where you need to 'create objects of similar types',
  * but the type of objects to be created can be decided at runtime. Delegate the instantiation logic.
-
+ * 
  */
 
 public class FactoryClass {
