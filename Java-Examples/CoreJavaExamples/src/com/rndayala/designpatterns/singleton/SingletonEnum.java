fb49c458499291de0881cd8442f3c54fdf9d44f0
Raghunath D <rndayala@gmail.com>
2023-07-20 09:46:13 +0530

    Singleton - enum implementation comments added

--- Java-Examples/CoreJavaExamples/src/com/rndayala/designpatterns/singleton/SingletonEnum.java
+++ Java-Examples/CoreJavaExamples/src/com/rndayala/designpatterns/singleton/SingletonEnum.java
@@ -1,6 +1,19 @@
 package com.rndayala.designpatterns.singleton;
 
+/**
+ * In the context of implementing the Singleton pattern using an enum, 
+ * the INSTANCE is a single constant instance of the enum type. 
+ * In Java, enum constants are implicitly static and final, 
+ * which means they can only be created once during the class loading and cannot be modified afterward. 
+ * As a result, an enum with a single constant effectively serves as a singleton.
+ * 
+ * @author rndayala
+ *
+ */
+
 public enum SingletonEnum {
+	// The INSTANCE is the only enum constant available for the SingletonEnum enum. 
+	// It represents the sole instance of the SingletonEnum class. 
 	INSTANCE;
 	
 	public void doSomething() {
@@ -16,3 +29,9 @@ public enum SingletonEnum {
 	}
 
 }
+
+// observations :
+// As enum constants are inherently thread-safe and guaranteed to be initialized only once, 
+// using an enum for the Singleton pattern eliminates the need for explicit synchronization and 
+// ensures a simple, efficient, and safe singleton implementation in Java.
+
