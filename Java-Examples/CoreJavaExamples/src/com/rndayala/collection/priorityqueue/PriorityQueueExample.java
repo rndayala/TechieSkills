f9b87206fb31954255d5367a7502f9af2a79718d
Raghunath D <rndayala@gmail.com>
2023-12-12 12:27:19 +0530

    Streams code added to example

--- Java-Examples/CoreJavaExamples/src/com/rndayala/collection/priorityqueue/PriorityQueueExample.java
+++ Java-Examples/CoreJavaExamples/src/com/rndayala/collection/priorityqueue/PriorityQueueExample.java
@@ -1,9 +1,14 @@
 package com.rndayala.collection.priorityqueue;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.function.Consumer;
 
 public class PriorityQueueExample {
 
+	static Integer sum = 0;
+	
 	public static void main(String[] args) {
 		PriorityQueue pq = new PriorityQueue();
 		pq.add(100);
@@ -36,5 +41,20 @@ public class PriorityQueueExample {
 //		pq.add(null);
 //		
 //		System.out.println(pq);
+		
+		// to use a value in anonymous classes/lambda expressions, across each iteration 
+		// it must be instance/static class level variable
+		List<Integer> nums =  Arrays.asList(4, 6, 1, 2, 3, 8, 7);
+		nums.forEach(i -> {
+			if (i%2 == 0) {
+				sum = sum + (i*2);
+			}
+		});
+		
+		System.out.println("Sum : " + sum);
+		
+		// Using streams
+		int sum_nums = nums.stream().filter(i-> i%2==0).map(i-> i*2).reduce(0,  (c, e) -> c+e);
+		System.out.println(sum_nums);
 	}
 }
